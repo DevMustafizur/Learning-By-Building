f001dda44ce69e28d312b8e4d36f4521de11355a
Mustafizur <mustafizurrahman1256@gmail.com>
2026-05-08 08:38:55 +0600

    updated: shop_page and old page

--- shopEasy/script.js
+++ shopEasy/script.js
@@ -3,11 +3,19 @@ const toggleCloseBtn = document.querySelector(".toggleCloseBtn")
 const menuList = document.querySelector(".menu_list")
 console.log(menuList)
 
-toggleOpenBtn.addEventListener("click", (e)=>{
+toggleOpenBtn.addEventListener("click", (e) => {
     e.preventDefault()
     menuList.classList.add("show")
 })
-toggleCloseBtn.addEventListener("click", (e)=>{
+toggleCloseBtn.addEventListener("click", (e) => {
     e.preventDefault()
     menuList.classList.remove("show")
+})
+
+
+// media query
+window.addEventListener("resize", () => {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        menuList.classList.remove("show");
+    }
 })
